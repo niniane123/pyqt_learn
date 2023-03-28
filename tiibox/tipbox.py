6e94e9837ca680f5018bfb27bb25673b23dd2c31
Niniane <2307784147@qq.com>
2023-03-28 21:33:58 +0800

    commit:setToolTip为button Qweigt添加提示框

---
+++ tiibox/tipbox.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication)
+from PyQt5.QtGui import QFont
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # setFont()这个静态方法设置了提示框的字体，我们使用了10px的SansSerif字体。
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        # 调用setToolTip()创建提示框可以使用富文本格式的内容。
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
