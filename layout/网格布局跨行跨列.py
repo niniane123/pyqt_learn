d33b75c4281d35b3f3589d494b77ee0994abe70f
Niniane <2307784147@qq.com>
2023-04-02 20:57:43 +0800

    commit:栅格布局跨行跨列

---
+++ layout/网格布局跨行跨列.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
+                             QTextEdit, QGridLayout, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+
+    def initUI(self):
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+# 我们可以指定组件的跨行和跨列的大小。这里我们指定这个元素跨5行显示
